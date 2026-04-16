367f250f1b8907b40cd1cfc476b79bc15ed3fe56
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-16 21:12:16 +0530

    Add files via upload

---
+++ shwet.py
@@ -0,0 +1,57 @@
+#python basic fundamental statements
+'''
+
+print("my name is shwet.")
+
+name=input("Enter your name :")
+
+print("my name is",name)
+
+'''
+'''
+#how to add two numbers
+
+a=10
+b=3
+
+sum=a+b
+
+print("the sum is :",sum)
+'''
+'''
+# simple calculator
+
+a= int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("Addition" ,a+b)
+print("Substraction" , a  - b)
+print("Multiplication" , a * b)
+print("Division" , a / b)
+'''
+'''
+#ice creame maker
+corn=input("choose your corn (wheat / mainda)")
+flavour=input("choose your flavour (vanila/chocolate/strawberry):")
+
+print("Here's your ice cream:")
+
+print ("[" + corn + "corn + " + flavour + " + rainbow glitter]")
+'''
+# pet talk simulator
+
+pet =input("what pet do you have(turtle/rabbit/dog):")
+
+if pet =="turtle":
+    print("bluh bluh ! i am lazy!!")
+elif pet=="rabbit":
+    print("shuh shuh ! i can run fast.")
+elif pet=="dog":
+    print("woof woof ! i want treats!!")
+else:
+    print("That's a unique pet!!!")
+          
+
+
+
+
