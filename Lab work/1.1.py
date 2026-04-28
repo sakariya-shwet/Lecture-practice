4d0f9222628bf327dba7694c33ef26ccd1bfaa86
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-28 09:34:52 +0530

    Add files via upload

---
+++ Lab work/1.1.py
@@ -0,0 +1,28 @@
+# install ptython
+
+#python --3.14.4
+
+
+'''# simple program
+name=input("Enter your name:-")
+age=input("Enter your age:-")
+school=input("Enter your school:-")
+
+print(f"your name is {name},your age is {age},your school is {school}")
+
+'''
+# output
+
+#* 
+#* * 
+#* * * 
+#* * 
+#* 
+
+rows=3
+
+for i in range (1,rows+1):
+    print("*"*i)
+
+for i in range (rows-1,0-1):
+    print("*"*i)
--- Lab work/1.2.py
+++ Lab work/1.2.py
@@ -1,4 +1,5 @@
 # using aep
+'''
 
 print("Apple","banana","Watermelon","mango",sep="|")
 
@@ -13,6 +14,71 @@ age=input("Enter your age")
 hobby=input("Enter your hobby")
 
 print(f"Hello my name is {name} my age is {age} my hobby is {hobby}")
+
+
+# Input two numbers
+num1 = float(input("Enter first number:- "))
+num2 = float(input("Enter second number:- "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+
+# Declare variables
+int = 100
+float = 3.14
+string = "Hello"
+boolean = True
+complex = 33 + 3j
+
+print(int, type(int))
+print(float, type(float))
+print(string, type(string))
+print(boolean, type(boolean))
+print(complex, type(complex))
+
+# Input height and weight
+height =input("Enter your height:- ")
+weight =input("Enter your weight:- ")
+
+print(f"Your height is {height} and your weight is {weight} ")
+
+# Input boolean values
+a =True
+b =False
+
+print("AND:", a and b)
+print("OR:", a or b)
+print("NOT a:", not a)
+print("NOT b:", not b)
+
+
+# assignment operator
+a = 10
+print("A value:", a)
+
+a += 10
+print("A value += 10:--", a)
+
+a -= 10
+print("A value -= 10:--", a)
+
+a *= 10
+print("A value *= 10:--", a)
+
+a /= 10
+print("A value /= 10:--", a)'''
+
+
+
+
+
+
+
 
 
 
