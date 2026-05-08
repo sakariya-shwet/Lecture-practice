62c88826abf005c9045634b64c0de12b94837398
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-08 11:54:15 +0530

    Add files via upload

---
+++ Lab work/2.2.py
@@ -0,0 +1,95 @@
+# LAB WORK 2.2
+
+'''print("q-1-->Maximum number:")
+
+print("\n")
+
+a=int(input("Enter first number:--"))
+b=int(input("Enter second number:--"))
+c=int(input("Enter third number:--"))
+
+
+if a>=b and a>=c:
+    print("Maximum is ",a)
+
+elif b>=a and b>=c:
+    print("Maximum is",b)
+
+else:
+     print("Maximum is ",c)
+
+
+print("q-2-->Minimun number:")
+
+print("\n")
+
+
+a=int(input("Enter first number:--"))
+b=int(input("Enter second number:--"))
+c=int(input("Enter third number:--"))
+
+
+if a<=b and a<=c:
+    print("Minimun is ",a)
+
+elif b<=a and b<=c:
+    print("Minimun is",b)
+
+else:
+     print("Minimun is ",c)
+
+
+
+print("q-3-->Maximum number:")
+
+print("\n")
+
+a = int(input("Enter first number:--"))
+b = int(input("Enter second number:--"))
+c = int(input("Enter third number:--"))
+d = int(input("Enter four number:--"))
+
+if a >= b and a >= c and a >= d:
+    print("Maximum is", a)
+
+elif b >= a and b >= c and b >= d:
+    print("Maximum is", b)
+
+elif c >= a and c >= b and c >= d:
+    print("Maximum is", c)
+
+else:
+    print("Maximum is", d)'''
+
+
+print("q-4-->switch case program:")
+
+print("\n")
+
+
+a=int(input("Enter first number:--"))
+b=int(input("Enter second number:--"))
+
+select=input("Enter the fnction ===>`+`,`-`,`*`,`\`")
+
+if choice==+:
+    print ("addition", a+b)
+if choice==`-`:
+    print ("substraction", a-b)
+if choice==`*`:
+    print ("multiplication", a*b)
+if choice==`\`:
+    print ("division", a/b)
+    
+
+
+
+
+
+
+
+
+
+
+
+
