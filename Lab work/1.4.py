4ea91670d6a074f128b438f07595b4a45ec99448
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-08 10:31:38 +0530

    Add files via upload

---
+++ Lab work/1.4.py
@@ -0,0 +1,55 @@
+# LAB WORK 1.4
+
+# CONFIGURE GIT:
+
+print("q-1-->configure git")
+
+print("\n")
+
+print("git config-global user name---> shwet sakaiya")
+print("git config-global user email---> sakaiyashwet@gmail.com")
+
+print("\n")
+
+
+
+print("q-2-->creating github account ")
+
+print("\n")
+
+print("Enter email--> username--> password--> complets setup")
+
+print("\n")
+
+
+print("q-3-->creating github repo ")
+
+print("\n")
+
+print("repo name--> description --> choose public\private--> and create a repo")
+
+print("\n")
+
+
+print("q-4-->push on github")
+
+print("\n")
+
+print("open terminal in the project folder--> initialize git and link github repo-->check your github repo")
+
+print("\n")
+
+print("q-5-->pull from github")
+
+print("\n")
+
+print("clone an existing repo-->modify a file on github --> in local repo,run git pull origin main--> verify your local copy has the uploded file")
+
+
+
+
+
+
+
+
+
