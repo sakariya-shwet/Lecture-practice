82df4fc50f1af7cdd7d89f72ef1b28f3275ed1c5
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-09 11:43:12 +0530

    Add files via upload

--- Lab work/3.1.py
+++ Lab work/3.1.py
@@ -6,7 +6,7 @@ print("\nq-->i.) simple program")
 name1 = input("Enter your first name: ")
 name2 = input("Enter your last name: ")
 
-print(f"Hello!! {name1} {name2}")'''
+print(f"Hello!! {name1} {name2}")
 
 
 print("\nq->> ii.) vegetable market")
@@ -28,6 +28,43 @@ elif choice == "3":
 
 else:
     print("Invalid choice")
+
+
+
+print("\nq->> iii.)str program")
+
+s=input("Enter a str")
+
+rev=s[::-1]
+
+print("reversed str",rev)
+
+if s==rev:
+    print("Palindrome")
+else:
+    print("Not palimdrome")
+
+
+
+print("\nq->> iv.)upper case and lowercase")
+
+s=input("Enter a str:--")
+
+print("Uppercase:--",s.upper())
+print("lowercase:--",s.lower())
+print("titlecase:--",s.title())'''
+
+
+print("\nq->> v.)upper case and lowercase")
+
+
+
+
+
+
+
+
+
 
 
 
