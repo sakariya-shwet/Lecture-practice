9fb4ef5c6d26ed4bedad22fa59bc88621f1ca8e2
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-09 11:28:12 +0530

    Add files via upload

---
+++ Lab work/3.1.py
@@ -0,0 +1,34 @@
+
+'''print("Welcome to lab work:-- 3.1")
+
+print("\nq-->i.) simple program")
+
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+
+print(f"Hello!! {name1} {name2}")'''
+
+
+print("\nq->> ii.) vegetable market")
+
+print("\n1.) Apple")
+print("2.) Banana")
+print("3.) Grapes")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("The price of Apple is 100₹")
+
+elif choice == "2":
+    print("The price of Banana is 50₹")
+
+elif choice == "3":
+    print("The price of Grapes is 80₹")
+
+else:
+    print("Invalid choice")
+
+
+
+
