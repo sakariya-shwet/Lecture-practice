b99fe6780608deef2acdf745afc6de998c7ccfb3
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-19 11:01:02 +0530

    Add files via upload

---
+++ 18-5-2026.py
@@ -0,0 +1,50 @@
+#opp (object oriented programming)
+# Mainly has 4 pillers
+
+print("i.) Encapsulation")
+print("ii.) Inheritence")
+print("iii.) Polymerphism")
+print("iv.) Abstraction")
+
+print("python objects & classes")
+
+print(" \n==>1ˢᵗ")
+print(" warpping data (in variables) and methods (functions) together inide a single class.")
+print("it also helps in data  hiding and security")
+print(" real time ex")
+print("==>think about ATM machine")
+print(" you can use ATM functions but internal banking details are hidden.")
+print("advantage data security,prevent direct access better control over data")
+
+print("\n==>2 nd")
+print(" one class can use properties")
+print("parent class features can be reused in child class")
+print(" father==> son")
+print(" surname")
+print(" properties")
+print(" features")
+print(" advantage")
+print(" code reusability less code duplication easy maintanance type of inheritence type of meaning")
+
+print("\n==>3 rd")
+print("same method name behave different")
+print("real time ex")
+print("father at home")
+print("employee at office")
+print("friend out side")
+
+print("advantage")
+print("flexibility,reusable methods clean code ")
+
+print("\n==>4 rd")
+print("hiding internal implementation and showing only important features ")
+print("advantage")
+print("ahides complexility better security clean code reusability")
+
+
+
+
+
+
+
+
