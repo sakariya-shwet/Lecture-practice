9a05e4e24aded3f0e0edac246c94455af43c2c88
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-21 11:31:57 +0530

    Add files via upload

---
+++ 20-5-2026.py
@@ -0,0 +1,66 @@
+# PYTHON OPP STUDENT LOGIC
+# TOPIC COVERED
+'''
+1.CLASS AND OBJECT
+2.SELF KEYWORD
+3.DEL KEYWORD
+4. ENCAPSULATION'''
+
+# CLASS AND OBJECT
+
+class car:
+    comp_name=None
+    model=None
+    color=None
+    year=None
+
+car1=car()
+car2=car()
+
+car1.comp_name="Tata"
+car1.model="sierra"
+car1.color="Yellow"
+car1.Year=2026
+
+car2.comp_name="Rolls Royce"
+car2.model="Rolls Royce phantom"
+car2.color="white"
+car2.Year=2025
+
+print("car1 details")
+print(car1.comp_name)
+print(car1.model)
+print(car1.color)
+print(car1.Year)
+
+print("\ncar2  details")
+print(car2.comp_name)
+print(car2.model)
+print(car2.color)
+print(car2.Year)
+
+# student
+
+class student data:
+    std_name=None
+    std_id=None
+    std_age=None
+    std_course=None
+
+student1=student data()
+student2=student data()
+
+student1.std_name="shwet"
+student1.std_id="shwet"
+student1.std_age="shwet"
+student1.std_age="shwet"
+
+
+
+
+
+ 
+ 
+
+
+
