5eb88580c172097b38f664bbac801eb2e45bda54
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-27 11:06:56 +0530

    Add files via upload

---
+++ 26-5-2026.py
@@ -0,0 +1,22 @@
+# polymwephism in python
+#mean object oriented programmimg is  many forms
+# it allows the same method name to perform different task depending on the object or argu used
+
+#1) overloding
+#2) overriding
+
+# it also provided bif
+
+# issubclass()
+# super()
+
+# 1) method overloding
+#mean creating multiple methods with same name but different parameters
+
+class calculator:
+    def multiply (self,a,b,c=1)
+    return a*b*c
+
+# the object
+
+calc=calculator()
