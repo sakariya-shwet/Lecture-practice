32b1b53ee725d8e960e0045ba8eb2456a41ebb75
sakariya-shwet <sakariyashwet@gmail.com>
2026-05-28 12:27:04 +0530

    Add files via upload

---
+++ program list.py
@@ -0,0 +1,63 @@
+print("<<<<welcome to python program list>>>>")
+
+print("\nQ ==>1")
+
+num=int(input("Enter a random number:=="))
+if num >0:
+        print("this number is positive num")
+
+print("\nQ==2")
+
+
+num=int(input("Enter a random number:=="))
+if num >0:
+        print("this number is positive num")
+else:
+    print("this num is negative")
+
+print("\nQ==3")
+
+day = int(input("Enter the day number (Sunday=1, Saturday=7): "))
+
+if day == 1:
+    print("It's Sunday")
+elif day == 2:
+    print("It's Monday")
+elif day == 3:
+    print("It's Tuesday")
+elif day == 4:
+    print("It's Wednesday")
+elif day == 5:
+    print("It's Thursday")
+elif day == 6:
+    print("It's Friday")
+elif day == 7:
+    print("It's Saturday")
+else:
+    print("Invalid choice! Please enter a number between 1 and 7.")
+
+print("\nQ==4")
+
+age=input("Enter your age :==")
+
+if age>="18":
+    if age>="60":
+        print("senior citizen age")
+    else:
+         print("Adult age")
+else:
+    print(" Minorr age")
+
+print("\nQ==5")
+
+
+
+
+
+
+
+
+
+
+
+    
