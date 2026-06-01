0642ad3adcc213724d16f194868d1ff30b622dd8
sakariya-shwet <sakariyashwet@gmail.com>
2026-06-01 11:46:12 +0530

    Add files via upload

--- program list.py
+++ program list.py
@@ -1,23 +1,27 @@
-print("<<<<welcome to python program list>>>>")
+print("<<<<print(Welcome! \U0001F44B to python program list>>>>")
 
-print("\nQ ==>1")
+print("\n")
+print("\U0001F539 Q ==>1")
 
-num=int(input("Enter a random number:=="))
+'''num=int(input("Enter a random number:=="))
 if num >0:
         print("this number is positive num")
 
-print("\nQ==2")
+print("\n")
+print("\U0001F539 Q==>2")
 
 
-num=int(input("Enter a random number:=="))
-if num >0:
-        print("this number is positive num")
+num=int(input("Enter a random number to check even or odd:=="))
+
+if num % 2 == 0:
+    print("this number is even")
 else:
-    print("this num is negative")
+    print("this number is odd")
 
-print("\nQ==3")
+print("\n")
+print("\U0001F539 Q==>3")
 
-day = int(input("Enter the day number (Sunday=1, Saturday=7): "))
+day = int(input("Enter Today number EX.(Sunday=1, Saturday=7): "))
 
 if day == 1:
     print("It's Sunday")
@@ -36,7 +40,8 @@ elif day == 7:
 else:
     print("Invalid choice! Please enter a number between 1 and 7.")
 
-print("\nQ==4")
+print("\n")
+print("\U0001F539 Q==>4")
 
 age=input("Enter your age :==")
 
@@ -46,9 +51,89 @@ if age>="18":
     else:
          print("Adult age")
 else:
-    print(" Minorr age")
+    print(" Minor age")
 
-print("\nQ==5")
+print("\n")
+print("\U0001F539 Q==>5")
+while True:
+        guess_game=int(input("\nEnter a letter (1-5) (game-guess)"))
+
+        match guess_game:
+                 case 1:
+                        print("Wrong gussed\U0001F614 you are so close enter another letter")
+                 case 2:
+                        print("Wrong gussed\U0001F614 you are so close enter another letter")
+                 case 3:
+                        print("Yes you guessed right!!")
+                        break
+                 case 4:
+                        print("Wrong gussed\U0001F614 you are so close enter another letter")
+                 case 5:
+                        print("Wrong gussed\U0001F614 you are so close enter another letter")
+
+print("\n")
+print("\U0001F539 Q==6")
+a=1
+while a<=10:
+        print(a)
+        a +=1
+
+print("\n")
+print("\U0001F539 Q==7")
+
+for i in range(1,15 +1):
+        print(i)
+        
+
+print("\n")
+print("\U0001F539 Q==8")
+
+for i in range(1,11,1):
+        print(i)
+
+print("\n")
+print("\U0001F539 Q==9")
+for i in range(1,11+1):
+        if i ==9:
+                break
+        print(i)
+
+print("\n")
+print("\U0001F539 Q==10")
+for i in range(1,11+1):
+        if i ==9:
+                continue
+
+        print(i)
+
+print("\n")
+print("\U0001F539 Q==11")
+for i in range(1,11+1):
+        if i ==9:
+                pass
+
+        print(i)
+'''
+print("\n")
+print("\U0001F539 Q==12")
+
+for i in range(1,4):
+        for j in range(1,4):
+                print(i,j)
+
+print("\n")
+print("\U0001F539 Q==13")
+                
+        
+
+
+        
+        
+        
+
+
+
+
 
 
 
